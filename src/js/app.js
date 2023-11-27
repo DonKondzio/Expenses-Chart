523658ee2d9631fa957bfa3f5be95f3b568e0df4
Konrad <konradkowalczyk07@gmail.com>
2023-11-27 18:40:32 +0100

    some mistakes fixed

--- src/js/app.js
+++ src/js/app.js
@@ -22,6 +22,8 @@ const showAmount = (e) => {
   e.target.firstChild.classList.add("active");
 };
 
+bars[new Date().getDay() - 1].classList.add("active");
+
 const hideAmount = (e) => {
   e.target.firstChild.classList.remove("active");
 };
@@ -30,47 +32,3 @@ bars.forEach((bar) => {
   bar.addEventListener("mouseover", showAmount);
   bar.addEventListener("mouseout", hideAmount);
 });
-
-// class HandleChart {
-//   constructor() {
-//     this.bars = document.querySelectorAll(".bar");
-//     this.amounts = [];
-//     this.init();
-//   }
-
-//   init() {
-//     const data = fetch("./data.json")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data);
-//       })
-//       .catch((error) => console.log(error));
-
-//     data.then((data) => {
-//       data.forEach((item) => console.log(item));
-//     });
-//   }
-
-//   showAmount = (index) => {};
-
-//   addAmountElements() {
-//     this.bars.forEach((bar, index) => {
-//       const amountContainer = document.createElement("div");
-//       const amountParagraph = document.createElement("p");
-//       amountContainer.classList.add("day-amount-container");
-//       amountParagraph.classList.add("day-amount");
-//       amountParagraph.textContent = `${this.amounts[index]}$`;
-//       amountContainer.appendChild(amountParagraph);
-//       // bar.appendChild(amountContainer);
-//       // console.log(this.amounts);
-//     });
-//   }
-
-//   addListeners = () => {
-//     this.bars.forEach((bar, index) =>
-//       bar.addEventListener("mouseover", showAmount(index))
-//     );
-//   };
-// }
-
-// const handleChart = new HandleChart();
